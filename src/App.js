4dd5f8149297cfdb5fbdb6f937e4ac7bd0b24bdc
Telbit <t.t.bende@gmail.com>
2020-11-25 19:05:20 +0100

    App.js refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
-import Grid from '@material-ui/core/Grid';
-import Card from './components/Cards/Card';
 import {Product} from './components/Product'
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {ProductList} from './components/ProductList';
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar/>
-        <div>
-          <Grid>
-            <Card></Card>
-            <Card reverse={true}></Card>
-          </Grid>
-        </div>
-        <Route path="/product/:id" component={Product} />
+        <Switch>
+          <Route path="/" exact component={ProductList} />
+          <Route path="/product/:id" component={Product} />
+        </Switch>
       </div>
     </Router>
   );
