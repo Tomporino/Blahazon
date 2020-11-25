8794bd33ccdc953047f022e16e984fd863be7cd2
Tomporino <tomorpeter97@gmail.com>
2020-11-25 15:07:35 +0100

    homepage

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -3,12 +3,17 @@ import Grid from '@material-ui/core/Grid';
 import {makeStyles} from '@material-ui/core/styles'
 import { Typography } from '@material-ui/core';
 import Image from 'material-ui-image';
+import testImage from '../../resources/Tshirts/whiteTshirt.png'
 
 const useStyles = makeStyles((theme) => ({
     container: {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(12, 1fr)',
-        gridGap: theme.spacing(3),
+        background: "blue"
+    },
+    lorem: {
+        padding: '50px'
+    },
+    image: {
+        padding: '10px'
     }
 }));
 
@@ -16,19 +21,32 @@ const useStyles = makeStyles((theme) => ({
 function Card() {
     const classes = useStyles();
     return (
+        <div className={classes.lorem}>
         
-        <Grid container>
-            <Grid item xs={6}>
-                <Typography variant="h2">
-                    Valami
-                </Typography>
+        <Grid container className={classes.container}
+            justify="space-evely"
+            alignItems="center"
+            spacing={3}>
+
+            <Grid item container xs={6} 
+                justify="center" 
+                spacing={3}>
+
+                <Grid item xs={6}>
+                    <Typography variant="p">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    </Typography>
+                </Grid>
+
             </Grid>
             <Grid item xs={6}>
                 <Image
-                    src="http://loremflickr.com/300/200"
-                />
+                    src={testImage}
+                    aspectRatio={(4/3)}
+                    />
             </Grid>
         </Grid> 
+        </div>
     );
 }
 
