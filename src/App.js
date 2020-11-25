660651c04b6a6ba6577ab9b7e1c543b4e1308841
J <J@DESKTOP-K8E0BDM>
2020-11-25 21:25:22 +0100

    merge?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css'
 const products = [
   {id:"1", imgsrc:`${testImage}`, name:"testkep", description:"leiras"},
   {id:"2", imgsrc:"kep.jpg", name:"testkep2", description:"leiras2"},
-  {id:"2", imgsrc:"kep.jpg", name:"testkep2", description:"leiras2"},
+  {id:"2", imgsrc:"kep.jpg", name:"testkep2 ez egy szöveg ami valahogy kinéz", description:"leiras2 es ez is kinez valahogy lol de jo lesz"},
   {id:"2", imgsrc:"kep.jpg", name:"testkep2", description:"leiras2"},
   {id:"2", imgsrc:"kep.jpg", name:"testkep2", description:"leiras2"},
   {id:"2", imgsrc:"kep.jpg", name:"testkep2", description:"leiras2"},
